c81310a4c5468b323b94b739d061655d0f5d09c5
gojutin <gojutin@gmail.com>
2018-01-27 17:27:23 -0500

    Updated dependencies, minor code tweaks and added meta tags and images.

--- src/components/AnimationFrame.js
+++ src/components/AnimationFrame.js
@@ -4,70 +4,78 @@ import { StyleSheet, css } from 'aphrodite';
 
 const styles = StyleSheet.create({
   animationFrame: {
-    textAlign: "center",
-    color: "#00C851",
-    width: 100 + "%",
-    height: 300 + "px",
-    position: "fixed",
-    background: "#2E2E2E",
+    textAlign: 'center',
+    color: 'lightgray',
+    width: 100 + '%',
+    height: 300 + 'px',
+    position: 'fixed',
+    background: '#2E2E2E',
     zIndex: 9999,
-    padding: 10 + "px",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
+    padding: 10 + 'px',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
   },
   replayButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
-    left: 45 + "%",
-    right: 45 + "%",
-    background: "none",
-    cursor: "pointer",
-    outline: "none",
-    padding: "0px 5px",
-    color: "#bdbdbd",
-    overflowY: "scroll",
+    left: 45 + '%',
+    right: 45 + '%',
+    background: 'none',
+    cursor: 'pointer',
+    outline: 'none',
+    padding: '0px 5px',
+    color: '#bdbdbd',
+    overflowY: 'scroll',
   },
   icon: {
-    fontSize: 7 + "em",
-    paddingTop: .6 + "em", 
+    fontSize: 7 + 'em',
+    paddingTop: 0.6 + 'em',
   },
   text: {
-    paddingTop: 100 + "px", 
+    paddingTop: 100 + 'px',
     fontSize: 30 + 'px',
     '@media (min-width: 750px)': {
-      paddingTop: 70 + "px",
+      paddingTop: 70 + 'px',
       fontSize: 80 + 'px',
     },
   },
   anchor: {
-    color: "white",
-    padding: "0px 5px",
-    ":hover": {
-      color:"lightgray",
-    }
-  }
+    color: 'teal',
+    padding: '0px 5px',
+    ':hover': {
+      color: 'lightgray',
+    },
+  },
 });
 
-const AnimationFrame = ({animations, inputValue, showMergeOptions, handleAnimation, stylesheet, icon, color}) => {
-
+const AnimationFrame = ({
+  animations,
+  inputValue,
+  showMergeOptions,
+  handleAnimation,
+  stylesheet,
+  icon,
+  color,
+}) => {
   const { currentAnimation, mergedAnimations } = animations;
 
   return (
     <div className={css(styles.animationFrame)}>
-      <p>Interactive demo for 
-        <a 
-          href="https://github.com/FormidableLabs/react-animations" 
-          rel="noopener noreferrer" 
+      <p>
+        Interactive demo for
+        <a
+          href="https://github.com/FormidableLabs/react-animations"
+          rel="noopener noreferrer"
           target="_blank"
           className={css(styles.anchor)}
         >
-          react-animations 
+          react-animations
         </a>
       </p>
-      <div style={{color}}>
-      { inputValue 
-        ? <h1 
-            style={{color}}
+      <div style={{ color }}>
+        {inputValue ? (
+          <h1
+            style={{ color }}
             className={`
               ${`
                 ${css(styles.text, stylesheet[currentAnimation])}
@@ -77,32 +85,28 @@ const AnimationFrame = ({animations, inputValue, showMergeOptions, handleAnimati
           >
             {inputValue}
           </h1>
-        : <i className={` 
+        ) : (
+          <i
+            className={` 
               fa
               ${icon}
               ${css(styles.icon, stylesheet[currentAnimation])}
             `}
-          />   
-      }
+          />
+        )}
       </div>
 
-      { mergedAnimations.length === 2 && 
-        <div 
-          className={css(styles.replayButton)} 
-          onClick={handleAnimation}
-        >
-          <i 
-            className="fa fa-repeat fa-2x"
-          />
+      {mergedAnimations.length === 2 && (
+        <div className={css(styles.replayButton)} onClick={handleAnimation}>
+          <i className="fa fa-repeat fa-2x" />
         </div>
-      }
-
+      )}
     </div>
   );
-}
+};
 
 AnimationFrame.propTypes = {
-  showMergeOptions: _.bool, 
+  showMergeOptions: _.bool,
   inputValue: _.bool,
   handleAnimation: _.func,
   stylesheet: _.object,
@@ -111,7 +115,6 @@ AnimationFrame.propTypes = {
     currentAnimations: _.object,
     mergedAnimations: _.array,
   }),
-}
-
+};
 
 export default AnimationFrame;
--- src/components/Input.js
+++ src/components/Input.js
@@ -5,25 +5,26 @@ import { StyleSheet, css } from 'aphrodite';
 
 const styles = StyleSheet.create({
   inputWrapper: {
-    height: 45 + "px",
-    marginBottom: 2 + "px",
+    height: 45 + 'px',
+    marginBottom: 2 + 'px',
   },
   input: {
-    borderRadius: 5 + "px",
-    fontSize: 1.3 + "em",
-    border: "1px solid lightgray",
-    padding: 5 + "px",
-    outline: "none",
-    width: 98 + "%",
-    textAlign: "center",
+    borderRadius: 5 + 'px',
+    fontSize: 1.3 + 'em',
+    border: '1px solid lightgray',
+    padding: 5 + 'px',
+    outline: 'none',
+    width: 98 + '%',
+    textAlign: 'center',
   },
   headShake: {
+    animation: 'headShake 10s forwards',
     animationName: headShake,
     animationDuration: '1s',
   },
-})                
+});
 
-const Input = ({inputValue, inputError, onChange, type, placeholder}) => 
+const Input = ({ inputValue, inputError, onChange, type, placeholder }) => (
   <div className={css(styles.inputWrapper)}>
     <input
       type={type}
@@ -33,13 +34,14 @@ const Input = ({inputValue, inputError, onChange, type, placeholder}) =>
       placeholder={placeholder}
     />
   </div>
+);
 
 Input.propTypes = {
   inputValue: _.string,
-  inputError: _.bool, 
-  onChange: _.func, 
+  inputError: _.bool,
+  onChange: _.func,
   type: _.string,
   placeholder: _.string,
-}
+};
 
 export default Input;
